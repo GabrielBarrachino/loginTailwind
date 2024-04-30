7a3f621082dcf83db01b13e5c8e35f7076530ff8
GabrielBarrachino <gabrielbarrachino@outlook.com>
2024-04-30 20:37:17 -0300

    primeiro

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      color: {
+        primaria: "#222"
+      }
+    },
+  },
+  plugins: [],
+}
+
